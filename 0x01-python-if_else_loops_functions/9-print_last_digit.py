384fcabca8decee6379f857ce4074bb5df36dcc1
haythmelmagraby <haythmelmagraby@gmail.com>
2024-01-16 11:30:06 -0800

    9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/ust/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        last = number % 10
+        print("{:d}".format(last), end="")
+    else:
+        last = -number % 10
+        print("{:d}".format(last), end="")
+    return last
