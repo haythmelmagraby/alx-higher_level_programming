2124e74ad5d5553d8841b0bc940c1fe101679dd0
haythmelmagraby <haythmelmagraby@gmail.com>
2024-01-17 10:53:44 -0800

    10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,18 +9,14 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *ls1 = list;
-	listint_t *ls2;
+	listint_t *ls2 = list;
 
 	while (ls1 != NULL && ls1->next != NULL)
 	{
-		ls2 = ls1->next;
-		while (ls2 != NULL && ls2->next != NULL)
-		{
-			if (ls2 == ls1)
-				return (1);
-			ls2 = ls2->next;
-		}
 		ls1 = ls1->next;
+		ls2 = ls2->next->next;
+		if (ls1 == ls2)
+			return (1)
 	}
 	return (0);
 }
