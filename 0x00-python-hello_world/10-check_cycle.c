daec1f9c240e8603544ea979e96f50cdac7f46e7
haythmelmagraby <haythmelmagraby@gmail.com>
2024-01-17 01:11:19 -0800

    10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ *check_cycle- check cycle in linked list
+ *@list: head of the linked list
+ *Return: 1 if have cycle or 0 if not
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *ls1 = list;
+	listint_t *ls2;
+
+	while (ls1 != NULL && ls1->next != NULL)
+	{
+		ls2 = ls1->next;
+		while (ls2 != NULL && ls2->next != NULL)
+		{
+			if (ls2->next == ls1->next)
+				return (1);
+			ls2 = ls2->next;
+		}
+		ls1 = ls1->next;
+	}
+	return (0);
+}
