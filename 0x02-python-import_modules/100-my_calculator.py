adf29a41944476fc10abb0260380fc4ffd233692
haythmelmagraby <haythmelmagraby@gmail.com>
2024-01-18 08:44:23 -0800

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import calculator_1 as calc
     import sys
     if len(sys.argv) > 2:
-        if len(sys.argv) > 3:
+        if len(sys.argv) == 4:
             print("Usage: ./100-my_calculator.py <a> <operator> <b>")
             exit(1)
         a = int(sys.argv[1])
