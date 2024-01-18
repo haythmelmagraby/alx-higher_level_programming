9f70eb2e4f6a195dcf4fe4be2c53998543b65a09
haythmelmagraby <haythmelmagraby@gmail.com>
2024-01-18 09:32:49 -0800

    102

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,6 @@
 #!/ust/bin/python3
 def magic_calculation(a, b):
    from magic_calculation_102 import add, sub
-   a = 0
-   b = 1
    if a < b:
        c = add(a, b)
         for i in range(4, 6):
