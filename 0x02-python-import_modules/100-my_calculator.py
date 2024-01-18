77ff1d99c4924c5e25db21515fce58ee3e792263
haythmelmagraby <haythmelmagraby@gmail.com>
2024-01-18 09:01:16 -0800

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
             print("{} {} {} = {}".format(a, op, b, calc.div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
+            exit(1)
