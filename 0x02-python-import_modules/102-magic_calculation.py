a650635845231196821ae9ccbded59ddb079d06e
haythmelmagraby <haythmelmagraby@gmail.com>
2024-01-18 09:31:41 -0800

    102

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/ust/bin/python3
+def magic_calculation(a, b):
+   from magic_calculation_102 import add, sub
+   a = 0
+   b = 1
+   if a < b:
+       c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
+
+    return 0;
