e63ad2e8c0261a014f58ea3602b9f0a0a879dad2
haythmelmagraby <haythmelmagraby@gmail.com>
2024-01-20 01:16:54 -0800

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if not my_string:
-        return None
     new_string = ""
     for i in range(len(my_string)):
         if my_string[i].lower() != 'c':
