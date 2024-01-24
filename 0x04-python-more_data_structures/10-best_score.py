e5d3a6b16795a262f607c1a63359fc0f744da674
haythmelmagraby <haythmelmagraby@gmail.com>
2024-01-24 11:30:21 -0800

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,9 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     max = 0
+    key_val = ""
     for key in iter(a_dictionary):
         if a_dictionary[key] > max:
             max = a_dictionary[key]
-    return max
+            key_val = key
+    return key_val
