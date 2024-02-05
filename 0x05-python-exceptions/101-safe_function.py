9cf616e40bc6f5e933f71c45ba6384033a9dbadf
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-05 13:32:16 -0800

    101

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
+    import sys
+    try:
+        arg = fct(*args)
+        return arg
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
+        return None
