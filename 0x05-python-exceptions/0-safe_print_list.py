88ae00a3acb05006996fd0f424cc534fdc3b40b5
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-05 12:07:13 -0800

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     counter = 0
     try:
         while counter is not x:
-            print(my_list[counter], end=' ')
+            print(my_list[counter], end='')
             counter += 1
     except IndexError:
         None
