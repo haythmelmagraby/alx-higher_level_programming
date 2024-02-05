54b5318b86d8497d5ff4d9442b40bd4c4babfcf9
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-05 13:17:15 -0800

    100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
+    result = True
+    try:
+        print("{:d}".format(value))
+    except Exception as ex:
+        print("Exception:",ex,file=sys.stderr)
+        result = False
+    return result
