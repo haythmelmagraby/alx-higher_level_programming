106123d0a3e6dd34c48576ec08bae253ac2d07b9
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-05 12:01:16 -0800

    first commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+    try:
+        while counter is not x:
+            print(my_list[counter], end=' ')
+            counter += 1
+    except:
+        None
+    print()
+    return counter
