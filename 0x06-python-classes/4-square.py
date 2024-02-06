8181469895a0316d73f3a9c9ed956bf909cb3b45
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-06 10:55:51 -0800

    4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Square Module """
+
+
+class Square:
+    """ Define a square """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value=0):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
