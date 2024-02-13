9fcfdc2c9083563b5eac233f669f6a2584212ed9
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-12 17:41:59 -0800

    9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -74,8 +74,8 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        """ Class method
+        ''' Class method
         Args:
-            size: size of the square
-        """
+            size: size of the square.
+        '''
         return cls(size, size)
