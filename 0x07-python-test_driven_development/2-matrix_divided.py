dbfa6928512ab1493823144883a0cc2e3e4b1f9b
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-13 17:27:50 -0800

    2

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Divide a matrix"""
+
+
+def matrix_divided(matrix, div):
+
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError("matrix must be a matrix" +
+                " (list of lists) of integers/floats")
+    if not isinstance(div, (float,int)):
+        raise TypeError("div must be a number")
+
+    for n in matrix:
+        if not isinstance(n, list) or len(n) == 0:
+            raise TypeError("matrix must be a matrix" +
+                " (list of lists) of integers/floats")
+        if len(matrix) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have"+
+                    " the same size")
+        for i in n:
+            if not isinstance(x, list) or len(x) == 0:
+                raise TypeError("matrix must be a matrix" +
+                    " (list of lists) of integers/floats")
+        return [[round(i / div, 2) for i in n] for n in matrix]
+
+if __name__ == "__main__":
+    import = doctest
+    doctest.testfile("tests/2-matrix_divided.txt")
