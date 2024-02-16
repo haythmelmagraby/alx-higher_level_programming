eaa0be6b4b38dc98894598ea8be4c1828f81850f
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-16 14:57:11 -0800

    5

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -13,4 +13,4 @@ def say_my_name(first_name, last_name=""):
 
 if __name__ == "__main__":
     import doctest
-    doctest.testfile("tests/3-say_my_name.txt")
+    doctest.testfile("tests/5-text_indentation.txt")
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,3 +3,15 @@
 
 
 def text_indentation(text):
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for d in ".?:":
+        text = (d + "\n\n").join([l.strip(" ")
+                for l in text.split(d)])
+        print(text, end="")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
