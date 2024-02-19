fbffcf26be17685ba5faafd1d2cad62ea1e1990c
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-19 04:33:08 -0800

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Module for BaseGeometry '''
+
+
+class BaseGeometry:
+    ''' BaseGeometry class '''
+    def area(self):
+        '''raises an Exception with the message'''
+        raise Exception('area() is not implemented')
