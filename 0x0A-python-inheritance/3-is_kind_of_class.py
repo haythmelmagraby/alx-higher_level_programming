d38e3e954511e9dd020566aa62445f7c3fe373b4
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-19 03:55:46 -0800

    3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Module Same class or inherit from'''
+
+
+def is_kind_of_class(obj, a_class):
+    ''' check if object an instance from a_class '''
+    return isinstance(obj, a_class)
