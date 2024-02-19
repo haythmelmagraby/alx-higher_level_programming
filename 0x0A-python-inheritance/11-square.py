4c31b4e27ab08d9f69901aba8e05c8ce900d4ce7
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-19 05:41:10 -0800

    11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Module for Square '''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    ''' Square is subclass to BaseGeometry'''
+    def __init__(self, size):
+        '''Square constructor'''
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        '''calc Square area'''
+        return self.__size ** 2
+
+    def __str(self):
+        '''string impelementation '''
+        return ["[Square] " + str(self.__size) + "/" + str(self.__size)]
