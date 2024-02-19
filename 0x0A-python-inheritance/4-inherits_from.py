7e1ed5c34db94261b39ac503fa317455ff9cec20
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-19 11:45:24 -0800

    4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     ''' check class that inherited (directly or indirectly) '''
-    return (isinstance(obj, a_class) and (type(obj) != a_class))
+    return isinstance(obj, a_class) and (type(obj) != a_class)
