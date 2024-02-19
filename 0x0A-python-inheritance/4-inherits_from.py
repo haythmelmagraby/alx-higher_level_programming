bbe0e759534253e9512d01716146d572f91950d2
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-19 03:59:02 -0800

    4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Module Lookup module '''
+
+
+def inherits_from(obj, a_class):
+    ''' check class that inherited (directly or indirectly) '''
+    return (isinstance(obj, a_class) and (type(obj) != a_class))
