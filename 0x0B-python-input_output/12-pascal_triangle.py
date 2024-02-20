e88c5cb2f5b2d483d4b6d0e2c2f130bb229c65a0
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-20 12:10:09 -0800

    12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Create a function"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers representing the Pascal’s triangle of n"""
+    if n <= 0:
+        return []
+    tr = [[1]]
+    while len(tr) != n:
+        trt = [1]
+        trn = tr[-1]
+
+        for t in range(len(trn) -1):
+            trt.append(trt[t] + trt[t +1])
+
+        trt.append(1)
+        tr.append(trt)
+
+    return tr
