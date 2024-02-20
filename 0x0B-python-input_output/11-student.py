f1414a4b7a72633b629550531377f1b467c1cedc
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-20 11:58:48 -0800

    11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+''' class Student Module '''
+
+
+class Student:
+    ''' class Student '''
+    def __init__(self, first_name, last_name, age):
+        ''' student constructor '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        ''' return json dictionary '''
+        try:
+            for i in attrs:
+                if type(i) not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        dic = dict()
+
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                dic[key] = value
+
+        return dic
+
+    def reload_from_json(self, json):
+        ''' that replaces all attributes of the Student instance '''
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
