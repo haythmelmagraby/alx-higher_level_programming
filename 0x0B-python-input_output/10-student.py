cc3c0878dba15fe8dd32d896d291d2080648b6ea
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-20 11:45:26 -0800

    10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" class Student Module """
+
+
+class Student:
+    """ represent student """
+    def __init__(self, first_name, last_name, age):
+        """ student constructor """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ return json dictionary """
+        try:
+            for i in attrs:
+                if type(i) not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        dic = dict()
+
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                dic[key] = value
+
+        return dic
