37fa93df7fa73eb36450d9ce66a9dae15f685f2c
haythm elmagraby <haythmelmagraby@gmail.com>
2024-02-25 01:47:57 -0800

    2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''Rectangel Module'''
+from models.base import Base
+
+class Rectangle(Base):
+    '''Content of Rectangle Class'''
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
