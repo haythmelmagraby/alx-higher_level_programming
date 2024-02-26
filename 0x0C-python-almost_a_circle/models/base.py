04d51fb4b999f051d0311ec481996200ff79aa2e
haythm elmagraby <haythmelmagraby@gmail.com>
2024-02-26 03:40:34 -0800

    19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 '''Base Module'''
 import json
+import os
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class Base:
@@ -13,9 +16,50 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objets
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         '''Dictionary to JSON'''
         if not list_dictionaries or list_dictionaries is None:
             return "[]"
         else:
             return dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''JSON string to file'''
+        if list_objs is not None:
+            list_objs = [ i.to_dictionary() for i in list_objs ]
+        
+        with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as my_file:
+            my_file.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''JSON string to dictionary'''
+        if not json_string or json_string is None:
+            return "[]"
+        else:
+            return loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''Dictionary to Instance'''
+        if cls is Square:
+            ins = Square(1)
+        elif cls is Rectangle:
+            ins = Reactangle(1, 1)
+        else:
+            ins = None
+
+        ins.update(**dictionary)
+        return ins
+
+    @classmethod
+    def load_from_file(cls):
+        '''File to instances'''
+        file = "{}.json".format(cls.__name__)
+        if path.isfile(file):
+            with.open(file, "r", encoding="utf-8") as my_file:
+                return [cls.create(**i) for i in cls.from_json_string(my_file.read())]
+        else:
+            return []
