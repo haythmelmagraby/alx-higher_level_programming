c5397c11c1316a1f00a220dcb878acf3b9c3c51a
haythmelmagraby <haythmelmagraby@gmail.com>
2024-03-26 06:07:02 -0700

    9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,8 +2,6 @@
 USE hbtn_0c_0;
 -- 9. Full creation
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-
--- INSERTION
 INSERT INTO second_table (id, name, score) VALUES
 (1, "john", 10),
 (2, "Alex", 3),
