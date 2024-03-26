bae18c061dfabe5198f1fb3548a0b4fc92cfa084
haythmelmagraby <haythmelmagraby@gmail.com>
2024-03-26 05:47:00 -0700

    102

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,3 @@
--- that displays the top 3 of cities temperature 
+-- that displays the top 3 of cities temperature
+USE hbtn_0c_0;
 SELECT city, AVG(value) AS avg_temp from temperatures where month in (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
