f5c61f13f5039f8f3300e88de4dfc91cf384b0cb
haythmelmagraby <haythmelmagraby@gmail.com>
2024-03-27 05:58:12 -0700

    103

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,5 +1,5 @@
 --  lists all genres in the database hbtn_0d_tvshows_rate by their rating
-SELECT name, SUM(rate) AS rating FROM tv_genres AS t
+SELECT t.name, SUM(r.rate) AS rating FROM tv_genres AS t
 INNER JOIN tv_show_genres as s on s.genre_id = t.id
 INNER JOIN tv_show_ratings AS r ON s.show_id = r.show_id
-GROUP BY name ORDER BY rating DESC;
+GROUP BY t.name ORDER BY rating DESC;
