c8d7c345df4b34a951f86df1b593802ac23ff5b1
haythmelmagraby <haythmelmagraby@gmail.com>
2024-03-27 05:34:46 -0700

    102

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,2 +1,5 @@
 -- ists all shows from hbtn_0d_tvshows_rate by their rating
-
+SELECT title, SUM(rate) AS rating FROM tv_shows AS t
+INNER JOIN tv_show_ratings AS r ON t.id = r.show_id
+GROUP BY titel
+ORDER BY rating DESC;
